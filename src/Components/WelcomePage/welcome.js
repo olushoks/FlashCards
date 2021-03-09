e0c536a65eec07cd4a429779f9985df10ba42a18
Olukayode <olukayodeshokunbi@gmail.com>
2021-03-08 21:15:24 -0500

    Created Welcome Page Component with an input field for user to enter their name

---
+++ src/Components/WelcomePage/welcome.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./welcome.css";
+
+function WelcomePage() {
+  return (
+    <div>
+      <h2>Flash Cardzzzzz</h2>
+      <p>Enter your name to continue...</p>
+      <input type="text" name="name"></input>
+      <input type="submit" value="Click" name="name"></input>
+    </div>
+  );
+}
+
+export default WelcomePage;
---
+++ src/Components/app.js
@@ -0,0 +1,12 @@
+import React from "react";
+import WelcomePage from "./WelcomePage/welcome";
+
+function App() {
+  return (
+    <div>
+      <WelcomePage />
+    </div>
+  );
+}
+
+export default App;
