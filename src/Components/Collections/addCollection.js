f9aaa915bc3727d1c23e78cd49e95a6ee89e11be
Olukayode <olukayodeshokunbi@gmail.com>
2021-03-10 08:25:39 -0500

    Modified addCollection function component to succesfully add new collection to the databbase

--- src/Components/Collections/addCollection.js
+++ src/Components/Collections/addCollection.js
@@ -1,6 +1,33 @@
+import axios from "axios";
 import React from "react";
 import "./addCollection.css";
 
-function AddCollection(props) {}
+function AddCollection() {
+  let newCollection = {};
+  const handleChange = (e) => {
+    e.preventDefault();
+    newCollection.title = e.target.value;
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await axios
+      .post("http://localhost:5000/api/collections", newCollection)
+      .then()
+      .catch();
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          name="new-collection"
+          placeholder="Enter Collection Title"
+          onChange={handleChange}
+        ></input>
+        <input type="submit" value="Submit"></input>
+      </form>
+    </div>
+  );
+}
 
 export default AddCollection;
