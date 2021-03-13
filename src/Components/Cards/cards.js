4ce2d8e9f93edd453bc2f8102e6bfbcecdf560ff
Olukayode <olukayodeshokunbi@gmail.com>
2021-03-13 15:11:32 -0500

    Refactored viewCollections component from been a function component to a class component

--- src/Components/Cards/cards.js
+++ src/Components/Cards/cards.js
@@ -3,20 +3,35 @@ import "./cards.css";
 
 function DisplayCards(props) {
   const cardsInCollection = props.cardsInCollection;
-  let cardCount = cardsInCollection.length;
+  const currentCollection = props.currentCollection;
+  let cardCount;
   let currentCard = 0;
+  let cardToRender;
+  //let cardToRender = <p>Render React Render: {props.title}</p>;
 
-  return cardsInCollection.length === 0 ? null : (
-    <div>
+  if (!cardsInCollection) {
+    return null;
+  }
+
+  if (cardsInCollection.length === 0) {
+    cardToRender = <div></div>;
+  }
+
+  if (cardsInCollection.length > 0) {
+    cardCount = cardsInCollection.length;
+    cardToRender = (
       <div>
-        <h6>{`FlashCard ${currentCard + 1} of ${cardCount}`}</h6>
+        <div>
+          <h6>{`FlashCard ${currentCard + 1} of ${cardCount}`}</h6>
+        </div>
+        <h4>{`${currentCollection.title} Collection`}</h4>
+        <i className="fas fa-chevron-circle-left"></i>
+        <p>{cardsInCollection[currentCard].question}</p>
+        <i className="fas fa-chevron-circle-right"></i>
       </div>
-      <h4>{`${cardsInCollection[currentCard].title} Collection`}</h4>
-      <i class="fas fa-chevron-circle-left"></i>
-      <p>{cardsInCollection[currentCard].question}</p>
-      <i class="fas fa-chevron-circle-right"></i>
-    </div>
-  );
+    );
+  }
+  return cardToRender;
 }
 
 export default DisplayCards;
--- src/Components/Collections/viewCollections.js
+++ src/Components/Collections/viewCollections.js
@@ -1,52 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import "./viewCollections.css";
 import DisplayCards from "../Cards/cards";
 
-function ShowCollections(props) {
-  let cardsInCurrentCollection;
-
-  // DISPLAY CARDS SUB DOCUMENTS IN CLICKED COLLECTION
-  const getCardsInCollection = (collection) => {
-    cardsInCurrentCollection = [];
-    // CHECK IF COLLECTION HAS CARDS SUBDOCUMENT
-    if (collection.cards.length === 0)
-      return console.log(`There are no cards in ${collection.title}`);
-
-    // PUSH CARDS SUBDOCUMENT ARRAY INTO CARDSINCURRENTCOLLECTION ARRAY
-    cardsInCurrentCollection.push(collection.cards);
-    // DESTRUCTURE ARRAY
-    [cardsInCurrentCollection] = cardsInCurrentCollection;
-    console.log(cardsInCurrentCollection);
-    console.log(cardsInCurrentCollection.length);
-  };
-
-  const displayCards = () => {
-    if (!cardsInCurrentCollection || cardsInCurrentCollection.length === 0)
-      return null;
-
-    return (
-      <div>
-        <DisplayCards cardsInCollection={cardsInCurrentCollection} />
-      </div>
-    );
-    // return cardsInCurrentCollection.length === 0 ? null : (
-    //   <div>
-    //     <DisplayCards cardsInCollection={cardsInCurrentCollection} />
-    //   </div>
-    // );
-  };
+class ShowCollections extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collections: props.collections,
+      activeCollection: {},
+      cardsInActiveCollection: [],
+    };
+  }
 
   // CHECK IF COLLECTIONS ARE IN DATABASE AND RETURN IF YES
-  const collections = props.collections;
-  if (collections.length === 0) {
-    return <div>No Collection Found</div>;
-  } else {
+  displayCollections = () => {
+    const { collections } = this.state;
+
+    if (collections.length === 0) {
+      return <div>No Collection Found</div>;
+    }
     const collection = collections.map((el, index) => {
       return (
         <div
           className="collection-display"
           key={index + 1}
-          onClick={() => getCardsInCollection(el)}
+          onClick={() => this.getCardsInCollection(el)}
         >
           <i className="fas fa-plus-square icon"></i>
           <h3>{el.title}</h3>
@@ -54,14 +32,105 @@ function ShowCollections(props) {
         </div>
       );
     });
+    return collection;
+  };
+
+  // GET CARDS SUB DOCUMENT IN CLICKED COLLECTION
+  getCardsInCollection = (clickedCollection) => {
+    // const { cardsInActiveCollection, activeCollection } = this.state;
+    this.setState({
+      activeCollection: clickedCollection,
+      cardsInActiveCollection: clickedCollection.cards,
+    });
+
+    // alert(`Clicked`);
+    // CHECK IF COLLECTION HAS CARDS SUBDOCUMENT
+    // if (cardsInActiveCollection.length === 0) {
+    //   return console.log(`There are no cards in ${activeCollection.title}`);
+    // }
+  };
+
+  render() {
     return (
       <div>
-        {collection}
-        {displayCards()}
-        {/* <DisplayCards cardsInCollection={cardsInCurrentCollection} /> */}
+        {this.displayCollections()}
+        <div>
+          <DisplayCards
+            cardsInCollection={this.state.cardsInActiveCollection}
+            currentCollection={this.state.activeCollection}
+          />
+        </div>
       </div>
     );
   }
 }
+
+// function ShowCollection(props) {
+//   let cardsInCurrentCollection = [];
+
+//   // GET CARDS SUB DOCUMENT IN CLICKED COLLECTION
+//   const getCardsInCollection = (collection) => {
+//     cardsInCurrentCollection = [];
+//     // CHECK IF COLLECTION HAS CARDS SUBDOCUMENT
+//     if (collection.cards.length === 0) {
+//       return console.log(`There are no cards in ${collection.title}`);
+//     }
+
+//     // PUSH CARDS SUBDOCUMENT ARRAY INTO CARDSINCURRENTCOLLECTION ARRAY
+//     cardsInCurrentCollection.push(collection.cards);
+//     // DESTRUCTURE ARRAY
+//     [cardsInCurrentCollection] = cardsInCurrentCollection;
+//     console.log(cardsInCurrentCollection);
+//     console.log(cardsInCurrentCollection.length);
+
+//     // return cardsInCurrentCollection;
+//   };
+
+//   // const displayCards = () => {
+//   //   if (!cardsInCurrentCollection || cardsInCurrentCollection.length === 0) {
+//   //     return null;
+//   //   } else {
+//   //     return (
+//   //       <div>
+//   //         <DisplayCards cardsInCollection={cardsInCurrentCollection} />
+//   //       </div>
+//   //     );
+//   //   }
+//   //   // return cardsInCurrentCollection.length === 0 ? null : (
+//   //   //   <div>
+//   //   //     <DisplayCards cardsInCollection={cardsInCurrentCollection} />
+//   //   //   </div>
+//   //   // );
+//   // };
+
+//   // CHECK IF COLLECTIONS ARE IN DATABASE AND RETURN IF YES
+//   const collections = props.collections;
+//   if (collections.length === 0) {
+//     return <div>No Collection Found</div>;
+//   } else {
+//     const collection = collections.map((el, index) => {
+//       return (
+//         <div
+//           className="collection-display"
+//           key={index + 1}
+//           onClick={() => getCardsInCollection(el)}
+//         >
+//           <i className="fas fa-plus-square icon"></i>
+//           <h3>{el.title}</h3>
+//           <div></div>
+//         </div>
+//       );
+//     });
+
+//     return (
+//       <div>
+//         {collection}
+//         <div>
+//           <DisplayCards cardsInCollection={cardsInCurrentCollection} />
+//         </div>
+//       </div>
+//     );
+//   }
+// }
 
 export default ShowCollections;
