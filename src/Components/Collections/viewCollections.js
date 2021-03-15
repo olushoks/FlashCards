91454be9b526b03d6622a9dc342c81ce77f65680
Olukayode <olukayodeshokunbi@gmail.com>
2021-03-14 20:01:06 -0400

    Created DELETE request to delete specific card from current collection

--- src/Components/Collections/viewCollections.js
+++ src/Components/Collections/viewCollections.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 import "./viewCollections.css";
 import AddCollection from "./addCollection";
 
@@ -24,11 +25,7 @@ class ShowCollections extends Component {
     }
     const collection = collections.map((el, index) => {
       return (
-        <div
-          className="collection-display"
-          // onClick={() => this.getCardsInCollection(el)}
-          key={index + 1}
-        >
+        <div className="collection-display" key={index + 1}>
           <div>
             <i
               className="fas fa-plus-square icon"
@@ -58,15 +55,6 @@ class ShowCollections extends Component {
         </div>
       ),
     });
-    // return (
-    //   <div>
-
-    //   <AddCollection
-    //     action="Add Card to Collection"
-    //     collectionId={collectionId}
-    //   />
-    //   </div>
-    // );
   };
 
   // GET CARDS SUB DOCUMENT IN CLICKED COLLECTION
@@ -77,54 +65,58 @@ class ShowCollections extends Component {
       cardCount: clickedCollection.cards.length,
       currentCard: 0,
     });
-
-    console.log(this.state.activeCollection);
-    console.log(this.state.cardsInActiveCollection);
   };
 
   // SHOW CARDS IN CURRENT COLLECTION
   showCardsInCollection = () => {
+    // CARDS IS NULL WHEN COMPONENT MOUNTS, PREVENT FROM RENDERING
     if (!this.state.cardsInActiveCollection) {
       return null;
     }
 
     if (this.state.cardsInActiveCollection.length === 0) {
       return (
-        <div>
+        <div className="no-cards-message">
           There are no cards in {this.state.activeCollection.title} Collection
         </div>
       );
     }
 
+    // RENDER IF THERE ARE CARDS SUBDOCUMENT(S) IN THE COLLECTION
     if (this.state.cardsInActiveCollection.length > 0) {
-      // let cardCount = this.state.cardsInActiveCollection.length;
-      // this.setState({ cardCount });
+      const {
+        currentCard,
+        cardCount,
+        activeCollection,
+        cardsInActiveCollection,
+      } = this.state;
 
       return (
         <div className="card-section">
           <div className="card-edit-delete-section">
-            <i className="fas fa-edit card-edit-delete-icon"></i>
-            <i className="fas fa-trash-alt card-edit-delete-icon"></i>
+            <i
+              className="fas fa-edit card-edit-delete-icon"
+              onClick={this.editCard}
+            ></i>
+            <i
+              className="fas fa-trash-alt card-edit-delete-icon"
+              onClick={this.deleteCard}
+            ></i>
           </div>
           <div>
             <h6 className="card-count-indicator">
-              FlashCard {this.state.currentCard + 1} of {this.state.cardCount}
+              FlashCard {currentCard + 1} of {cardCount}
             </h6>
           </div>
           <h4 className="collection-name">
-            {this.state.activeCollection.title} Collection
+            {activeCollection.title} Collection
           </h4>
           <div>
             <i
               className="fas fa-chevron-circle-left next-button"
               onClick={this.previousCard}
             ></i>
-            <span>
-              {
-                this.state.cardsInActiveCollection[this.state.currentCard]
-                  .question
-              }
-            </span>
+            <span>{cardsInActiveCollection[currentCard].question}</span>
             <i
               className="fas fa-chevron-circle-right next-button"
               onClick={this.nextCard}
@@ -138,25 +130,53 @@ class ShowCollections extends Component {
   // GO TO PREVIOUS CARD
   previousCard = () => {
     let { currentCard, cardCount } = this.state;
-    currentCard--;
-    this.setState({ currentCard: currentCard });
-    console.log(`Go to Next: ${currentCard}`);
-    if (currentCard === 1) {
-      this.setState({ currentCard: cardCount });
+    if (currentCard >= 0) {
+      currentCard--;
+      this.setState({ currentCard: currentCard });
+      console.log(`Go to Next: ${currentCard}`);
     }
+    // if (currentCard === 1) {
+    //   this.setState({ currentCard: cardCount });
+    // }
   };
 
   // GO TO NEXT CARD
   nextCard = () => {
     let { currentCard, cardCount } = this.state;
-    currentCard++;
-    this.setState({ currentCard: currentCard });
-
-    console.log(`Go to Next: ${currentCard}`);
-
-    if (currentCard === cardCount) {
-      this.setState({ currentCard: 1 });
+    if (currentCard <= cardCount - 1) {
+      currentCard++;
+      this.setState({ currentCard: currentCard });
+      console.log(`Go to Next: ${currentCard}`);
     }
+
+    // if (currentCard === cardCount) {
+    //   this.setState({ currentCard: 1 });
+    // }
+  };
+
+  // DELETE CARD FROM COLLECTION
+  deleteCard = async () => {
+    const {
+      activeCollection,
+      cardsInActiveCollection,
+      currentCard,
+    } = this.state;
+
+    await axios
+      .delete(
+        `http://localhost:5000/api/collections/${activeCollection._id}/cards/${cardsInActiveCollection[currentCard]._id}`
+      )
+      .then()
+      .catch((err) => err);
+
+    console.log(
+      `Active Collections ID: ${activeCollection._id} || Cuurent Card's ID:${cardsInActiveCollection[currentCard]._id} `
+    );
+  };
+
+  // EDIT CARD IN COLLECTION
+  editCard = () => {
+    alert(`Edit me`);
   };
 
   render() {
